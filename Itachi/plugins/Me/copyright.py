0e5f9bc2e6f8b7dd6b241f842ca4f4551ab646a1
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-04 14:54:05 +0530

    Update copyright.py

--- Itachi/plugins/Me/copyright.py
+++ Itachi/plugins/Me/copyright.py
@@ -1,24 +1,51 @@
+from Itachi import Itachi as app
 from pyrogram import Client, filters
-from Itachi import app
-from pyrogram.types import Message
-
-# -----------------------------------------------------
-
-@app.on_edited_message(filters.group & ~filters.me)
-async def delete_edited_messages(client, edited_message):
-    await edited_message.delete()
 
 
-# -------------------------------
 
-async def delete_pdf_files(client, message):
-    if message.document and message.document.mime_type == "application/pdf":
-        warning_message = f"@{message.from_user.username} ᴍᴀᴀ ᴍᴀᴛ ᴄʜᴜᴅᴀ ᴘᴅғ ʙʜᴇᴊ ᴋᴇ,\n ʙʜᴏsᴀᴅɪᴋᴇ ᴄᴏᴘʏʀɪɢʜᴛ ʟᴀɢʏᴇɢᴀ \n\n ᴅᴇʟᴇᴛᴇ ᴋᴀʀ ᴅɪʏᴀ ᴍᴀᴅᴀʀᴄʜᴏᴅ.\n\n ᴀʙ @Itachi007ff ʙʜᴀɪ ᴋᴇ ᴅᴍ ᴍᴇ ᴀᴘɴɪ ᴍᴜᴍᴍʏ ᴋᴏ ʙʜᴇᴊ ᴅᴇ 🍌🍌🍌."
-        await message.reply_text(warning_message)
+async def delete_links(message):
+    if any(link in message.text for link in ["http", "https", "www."]):
         await message.delete()
-    else:  
-        pass
 
-@app.on_message(filters.group & filters.document)
-async def message_handler(client, message):
-    await delete_pdf_files(client, message)
+@app.on_message(filters.group & filters.text & ~filters.me)
+async def handle_messages(client, message):
+    await delete_links(message)
+
+@app.on_edited_message(filters.group & filters.text & ~filters.me)
+async def handle_edited_messages(client, edited_message):
+    await delete_links(edited_message)
+
+
+#---------------------------------------------------------------------------
+keywords_to_delete = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+@app.on_message(filters.group & filters.text & ~filters.me)
+async def delete_links(client, message):
+    if any(keyword in message.text for keyword in keywords_to_delete) and len(message.text.split()) > 20:
+        await message.delete()
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+@app.on_edited_message(filters.group & filters.text & ~filters.me)
+async def delete_edited_links(client, edited_message):
+    if any(keyword in edited_message.text for keyword in keywords_to_delete) and len(edited_message.text.split()) > 20:
+        await edited_message.delete()
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+
+@app.on_message(filters.group & filters.text & ~filters.me)
+async def delete_long_messages(client, message):
+    if len(message.text.split()) >= 20:
+        await message.delete()
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+@app.on_edited_message(filters.group & filters.text & ~filters.me)
+async def delete_edited_long_messages(client, edited_message):
+    if len(edited_message.text.split()) >= 20:
+        await edited_message.delete()
+
+
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
